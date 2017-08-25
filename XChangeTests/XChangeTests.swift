d4b92089fc4114d8aa8c25372143808e774be224
Nick Voloshyn <nikaladze@aol.com>
2017-08-25 13:57:09 +0300

    Initial Commit

---
+++ XChangeTests/XChangeTests.swift
@@ -0,0 +1,36 @@
+//
+//  XChangeTests.swift
+//  XChangeTests
+//
+//  Created by Nick Voloshyn on 8/25/17.
+//  Copyright © 2017 Bridge 777. All rights reserved.
+//
+
+import XCTest
+@testable import XChange
+
+class XChangeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
